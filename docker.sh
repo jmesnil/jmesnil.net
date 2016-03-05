06deadb1843092823b9da698827802e5d0a86fcf
Jeff Mesnil <jmesnil@gmail.com>
2016-03-05 14:38:43 +0100

    Use jmesnil/jmesnil.net Docker image

--- docker.sh
+++ docker.sh
@@ -5,4 +5,4 @@ AWS_ACCESS_KEY_ID=`s3cmd --dump-config | grep access_key | grep -oE '[^ ]+$'`
 AWS_SECRET_ACCESS_KEY=`s3cmd --dump-config | grep secret_key | grep -oE '[^ ]+$'`
 
 # ... and pass it to the s3cmd inside Docker using env variables
-docker run -it --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY -v `pwd`/:/home/jmesnil/jmesnil.net -p 4242:4242 -w /home/jmesnil/jmesnil.net jmesnil.net
+docker run -it --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY -v `pwd`/:/home/jmesnil/jmesnil.net -p 4242:4242 -w /home/jmesnil/jmesnil.net jmesnil/jmesnil.net
