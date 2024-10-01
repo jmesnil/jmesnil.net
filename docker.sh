ce5ec3bf3f2c6ec5a526c319fd8903beac1eb537
Jeff Mesnil <jmesnil@redhat.com>
2024-10-01 14:27:37 +0000

    Mount ~/.gitconfig when running the container
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- docker.sh
+++ docker.sh
@@ -5,4 +5,11 @@ AWS_ACCESS_KEY_ID=`s3cmd --dump-config | grep access_key | grep -oE '[^ ]+$'`
 AWS_SECRET_ACCESS_KEY=`s3cmd --dump-config | grep secret_key | grep -oE '[^ ]+$'`
 
 # ... and pass it to the s3cmd inside Docker using env variables
-docker run -it --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY -v `pwd`/:/home/dev/jmesnil.net -p 4242:4242 -w /home/dev/jmesnil.net jmesnil/jmesnil.net
+docker run -it --rm \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY \
+    -v `pwd`/:/home/dev/jmesnil.net \
+    -v $HOME/.gitconfig:/home/dev/.gitconfig \
+    -w /home/dev/jmesnil.net \
+    -p 4242:4242 \
+    jmesnil/jmesnil.net
